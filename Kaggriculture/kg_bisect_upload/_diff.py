a5215a9197e8dace58f31cd515f24eaf3d90fce9
Vineet <vineetdairashri5@gmail.com>
2026-08-20 20:49:25 +0200

    Cleanup: Move unused files to archive

--- Kaggriculture/kg_bisect_upload/_diff.py
+++
@@ -1,7 +0,0 @@
-import difflib
-a = open('main.py').readlines()
-b = open('v97_cap70.py').readlines()
-diffs = list(difflib.unified_diff(a, b, fromfile='main.py', tofile='v97_cap70.py', lineterm='', n=2))
-for d in diffs:
-    print(d)
-print(f"\nTotal diff lines: {len(diffs)}")
